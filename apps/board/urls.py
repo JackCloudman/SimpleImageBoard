ac2b9c76250d6d095180c654536b9ecc81db0f6f
jackcloudman <maylo360xd@gmail.com>
2018-03-04 00:27:46 -0600

    Modelos de iniciar sesion

--- apps/board/urls.py
+++ apps/board/urls.py
@@ -1,6 +1,9 @@
 from . import views
 from django.urls import path,include                                                                     
 from django.conf.urls import url
+from django.contrib.auth.views import login,logout
 urlpatterns = [
     url(r'^$', views.index),
+    url(r'^login/$',login,{'template_name':'login.html'}),
+    url(r'^logout/$', logout, {'next_page':'/'}),
 ]  
