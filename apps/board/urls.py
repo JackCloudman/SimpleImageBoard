264f3898e272e3b9fc7b00a0ebe123188ba6dd6d
jackcloudman <maylo360xd@gmail.com>
2018-03-08 00:37:51 -0600

    Creacion de la vista perfil! y creacion del modelo Profile

--- apps/board/urls.py
+++ apps/board/urls.py
@@ -2,8 +2,10 @@ from . import views
 from django.urls import path,include                                                                     
 from django.conf.urls import url
 from django.contrib.auth.views import login,logout
+from django.contrib.auth.decorators import login_required
 urlpatterns = [
     url(r'^$', views.index),
     url(r'^login/$',login,{'template_name':'login.html'}),
     url(r'^logout/$', logout, {'next_page':'/'}),
+    url(r'^profile/$',login_required(views.profile)),
 ]  
--- apps/board/views.py
+++ apps/board/views.py
@@ -3,3 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request,'board/index.html',{})
+def profile(request):
+    return render(request,'profile.html',{})
